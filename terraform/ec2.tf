97a6ede5d1d567b7a54e65a3d6be935bbc85c7d0
manthan <zeus@Zeus.localdomain>
2026-04-21 15:46:28 +0000

    terraform file error fixed

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,7 +1,12 @@
+resource "aws_key_pair" "my_key" {
+  key_name   = "my-key"
+  public_key = file("my-key.pub")
+}
 resource "aws_instance" "company_ec2" {
     ami = "ami-098e39bafa7e7303d"
     instance_type = "t2.micro"
     subnet_id = aws_subnet.public_subnet_1.id
+    key_name = aws_key_pair.my_key.key_name
     vpc_security_group_ids = [aws_security_group.ec2_sg.id]
     tags = {
         name = "company-server" 
@@ -11,6 +16,7 @@ resource "aws_instance" "bureau_ec2" {
     ami = "ami-098e39bafa7e7303d"
     instance_type = "t2.micro"
     subnet_id = aws_subnet.public_subnet_1.id
+    key_name = aws_key_pair.my_key.key_name
     vpc_security_group_ids = [aws_security_group.ec2_sg.id]
     tags = {
         name = "bureau-server"
@@ -20,6 +26,7 @@ resource "aws_instance" "employee_ec2" {
     ami = "ami-098e39bafa7e7303d"
     instance_type = "t2.micro"
     subnet_id = aws_subnet.public_subnet_2.id
+    key_name = aws_key_pair.my_key.key_name
     tags = {
         name = "employee-server"
         Tenant = "employee"  }
